e2b5b6bebbfafce93be2bb0db54e497f4176ccaf
Horațiu Udrea <udrea.horatiu@gmail.com>
2019-10-27 20:39:59 +0200

    Finished Lab 2

--- lab1.pl
+++ lab1.pl
@@ -1,8 +1,9 @@
+% containedIn(E: element, L: list)
 % flow (i, i)
-containedIn(_, []):-false.
 containedIn(E, [H|_]):-E == H.
 containedIn(E, [H|L]):-E\=H, containedIn(E, L).
 
+% listLength(L: list, LEN: length of the list)
 % flow (i, o)
 listLength([], 0).
 listLength([_|L], LEN):-length(L, LEN2), LEN is LEN2 + 1.

14676ff42b52a44a786f6166abc81e86eb492b41
Horațiu Udrea <udrea.horatiu@gmail.com>
2019-11-11 08:55:34 +0200

    Finished lab 4

--- lab4.pl
+++ lab4.pl
@@ -1,13 +1,25 @@
+% The list a1, ..., an is given and it consists of distinct integers. Write a predicate to determine all subsets
+  % with aspect of "mountain" (a set has a "mountain" aspect if the elements increase to a certain point
+  % and then decrease).
+% mountainSets(L: input list, S: list of sets)
 % flow (i, o)
-powerset(S, L):-findall(R, (subset(S, R), mountain(R)), L).
-subset([], []).
-subset([_|T], R):-subset(T, R).
-subset([H|T], [H|R]):-subset(T, R).
+mountainSets(L, S):-findall(R, (subset(L, R), mountain(R)), S).
 
-%flow (i)
+% Computes the subsets of the list
+% subset(L: input list, S: a subset of the list)
+% flow(i, o)
+subset([], []).
+subset([H|T], [H|R]):-subset(T, R).
+subset([_|T], R):-subset(T, R).
+
+% Checks if a list has a mountain aspect
+% mountain(L: input list)
+% flow (i)
 mountain(L):-mountainAspect(L, none).
 
-%flow(i, i)
+% Checks if a list has a mountain aspect using an atom representing the state.For the first call, use 'none'
+% mountainAspect(L: list, A: atom representing the state of the check)
+% flow(i, i)
 mountainAspect([H1, H2|T], A):- A = none, H1 < H2, !, mountainAspect([H2|T], up).
 mountainAspect([H1, H2|T], A):- A = up, H1 < H2, !, mountainAspect([H2|T], up).
 mountainAspect([H1, H2|T], A):- A = up, H1 > H2, !, mountainAspect([H2|T], down).

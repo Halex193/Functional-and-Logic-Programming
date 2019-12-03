3ed6179ee30bce714553dc009af370d85ab8d915
Horațiu Udrea <udrea.horatiu@gmail.com>
2019-12-03 19:44:35 +0200

    Finished LISP lab

--- lab1.lisp
+++ lab1.lisp
@@ -3,7 +3,7 @@
   (COND
     ((NULL a) b)
     ((NULL b) a)
-    ((< (CAR a) (CAR b))(CONS (CAR a) (mergeLists (CDR a) b)))
+    ((< (CAR a) (CAR b)) (CONS (CAR a) (mergeLists (CDR a) b)))
     (t (CONS (CAR b) (mergeLists a (CDR b))))
   )
 )
@@ -31,26 +31,57 @@
 
 ;(write (replaceElement 1 '((4 1) 2 1 3) '(12)))
 
-;9 c returns number
-(DEFUN decimalSum (a b) (decimalSum2 (REVERSE a) (REVERSE b) 1))
-
-(DEFUN decimalSum2 (a b p)
-  (COND
-    ((AND (NULL a) (NULL b)) 0)
-    (t (+ (* (+ (COND ((NULL a) 0) (t (CAR a))) (COND ((NULL b) 0) (t (CAR b)))) p) (decimalSum2 (CDR a) (CDR b) (* p 10))))
-  )
-)
-
-;(write (decimalSum '(1 1) '(1 4 6)))
-
 ;9 c weird
-(DEFUN decimalSum3 (a b) (REVERSE (decimalSum2 (REVERSE a) (REVERSE b) 0)))
+(DEFUN decimalSum (a b) (REVERSE (decimalSum2 (REVERSE a) (REVERSE b) 0)))
 
 (DEFUN decimalSum2 (a b c)
   (COND
-    ((AND (NULL a) (NULL b)) nil)
-    (t (CONS (+ (COND ((NULL a) 0) (t (CAR a))) (COND ((NULL b) 0) (t (CAR b))) c) (decimalSum2 (CDR a) (CDR b) (/ (+ (COND ((NULL a) 0) (t (CAR a))) (COND ((NULL b) 0) (t (CAR b))) c) 10))))
+    ((AND (NULL a) (NULL b) (= c 0) ) nil)
+    ((AND (NULL a) (NULL b)) (LIST c))
+    (t
+      (CONS
+        (mod (+
+               (COND ((NULL a) 0) (t (CAR a)))
+               (COND ((NULL b) 0) (t (CAR b)))
+               c
+             )
+          10
+        )
+
+        (decimalSum2
+          (CDR a)
+          (CDR b)
+          (div (+
+                 (COND ((NULL a) 0) (t (CAR a)))
+                 (COND ((NULL b) 0) (t (CAR b)))
+                 c) 10
+          )
+        )
+      )
+    )
   )
 )
 
-;(write (decimalSum '(1 1) '(1 1)))
+(DEFUN div (a b)
+  (/ (- a (mod a b)) b)
+)
+
+;(write (decimalSum '(1 0 0) '(1)))
+
+(DEFUN gcdim (a b)
+  (COND
+    ((= b 0) a)
+    (t (gcdim b (mod a b)))
+  )
+)
+
+;(write (gcdim 10 5))
+
+(DEFUN gcdlist (l)
+  (COND
+    ((= (LENGTH l) 1) (CAR l))
+    (t (gcdim (CAR l) (gcdlist (CDR l))))
+  )
+)
+
+;(write (gcdlist '(230 400)))
